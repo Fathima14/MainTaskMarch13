5381058208165fb1ea19cf5cee4d24ff637fef98
Bhuvana1 <deepakbhuvana@gmail.com>
2016-04-02 21:04:21 -0700

    added reversestring.java file

---
+++ src/ReverseString.java
@@ -0,0 +1,17 @@
+
+//java revese string program
+
+public class ReverseString{
+
+public static void main (String arg[])
+
+{
+	
+	ReverseString rs = new ReverseString();
+    rs.findreverse("welcome");
+    rs.StringBuffer1("house");
+    System.out.println("Result: "+rs.reverseR("Java2novice"));
+   
+        
+}
+}
