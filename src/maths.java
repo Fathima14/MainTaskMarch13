1a1c53423f1875530dc6612bf85ef2ed7f7728db
Fathima Sifa Syed Musthafa <fsifa14@gmail.com>
2016-04-02 21:04:11 -0700

    Assignment 1 Question B first 2 steps done

---
+++ src/maths.java
@@ -0,0 +1,14 @@
+public class Maths {
+
+    double add(double a, double b) {
+        double answer = a+b;
+        return answer;          
+    }
+# A new method is added for modifying a file
+   double subtract(double a, double b) {
+        double answer = a-b;
+        return answer;          
+    }
+
+}
+
