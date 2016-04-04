626fbde7ce0f547824230b1e21b3fedf97c756b6
Bhuvana1 <deepakbhuvana@gmail.com>
2016-04-03 17:00:21 -0700

    testing stash pop command

--- src/ReverseString.java
+++ src/ReverseString.java
@@ -11,9 +11,10 @@ public static void main (String arg[])
     rs.findreverse("welcome");
     rs.StringBuffer1("house");
     System.out.println("Result: "+rs.reverseR("Java2novice"));
-
+//squash and pull changes
 system.out.println("Testing squash and pull the changes command");
 System.out.println("This is added in branch");
         
+       
 }
 }
