6da7610b252f9a1d6f5bb15400310c5c901eebf0
Bhuvana1 <deepakbhuvana@gmail.com>
2016-04-06 15:56:21 -0700

    added Fnone

---
+++ src/gitMerging/someClass.java
@@ -0,0 +1,14 @@
+package gitMerging;
+
+public class someClass {
+	public void fnOne(){
+		System.out.println("I couldn't repair your brakes,so I made your horn louder");
+	}
+	public static void main(String[] args){
+	someClass sc = new someClass();
+	sc.fnOne();
+		
+		
+	}
+
+}
