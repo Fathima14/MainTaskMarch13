d8f2ff42e4cb8c0092ba0b7109047f657903fad5
Bhuvana1 <deepakbhuvana@gmail.com>
2016-04-08 17:59:05 -0700

    Added Events

--- src/gitMerging/gitMerge/src/gitMerge/SomeClass.java
+++ src/gitMerging/gitMerge/src/gitMerge/SomeClass.java
@@ -4,9 +4,13 @@ public class SomeClass {
 	public void thoughtForTheDay(){
 		System.out.println("Welcome");
 	}
+	public void upcomingEvents(){
+		System.out.println("Bithday party");
+	}
 	public static void main(String[] args){
 		SomeClass sc = new SomeClass();
 		sc.thoughtForTheDay();
+		sc.upcomingEvents();
 	}
 
 }
